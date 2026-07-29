6b5b266d856fc812177cb38a784246329bb0feb1
Tikktaalikk <rivallainjoa@gmail.com>
2026-07-29 11:10:44 +0200

    ajout de 8 oiseaux

--- games/oiseaux-photo/birds.js
+++ games/oiseaux-photo/birds.js
@@ -3,5 +3,13 @@ const BIRDS = [
   { id: 2, name: "Mésange bleue", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Cyanistes_caeruleus_Blue_Tit.jpg?width=500" },
   { id: 3, name: "Merle noir", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Merle_noir_ile_de_france.jpg?width=500" },
   { id: 4, name: "Moineau domestique", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Passer_domesticus_detailed.jpg?width=500" },
-  { id: 5, name: "Pigeon ramier", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Wood_pidgeon_Columba_palumbus.jpg?width=500" }
+  { id: 5, name: "Pigeon ramier", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Wood_pidgeon_Columba_palumbus.jpg?width=500" },
+  { id: 6, name: "Mésange charbonnière", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Parus_major_Luc_Viatour.jpg?width=500" },
+  { id: 7, name: "Chardonneret élégant", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Carduelis_carduelis_close_up.jpg?width=500" },
+  { id: 8, name: "Pinson des arbres", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Male_Chaffinch_-_Fringilla_coelebs.jpg?width=500" },
+  { id: 9, name: "Étourneau sansonnet", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Star_Sturnus_vulgaris.jpg?width=500" },
+  { id: 10, name: "Corneille noire", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Carrion_Crow_aka_Corvus_corone.jpg?width=500" },
+  { id: 11, name: "Pie bavarde", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Pica_pica1.jpg?width=500" },
+  { id: 12, name: "Tourterelle turque", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Streptopelia_decaocto_-_Eurasian_Collared_Dove_05.jpg?width=500" },
+  { id: 13, name: "Verdier d'Europe", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Greenfinch_Carduelis_chloris.jpg?width=500" }
 ];
